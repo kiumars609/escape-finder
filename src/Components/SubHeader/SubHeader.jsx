fb2e56a41494288684096860ca21c657d94a4a34
Kiumars609 <qumarsgamer@gmail.com>
2026-03-06 23:57:16 +0400

    Edit Button Section for SubHeader

--- src/Components/SubHeader/SubHeader.jsx
+++ src/Components/SubHeader/SubHeader.jsx
@@ -8,7 +8,7 @@ export default function SubHeader() {
     <>
       <div className="container-fluid sub-header">
         <div className="row">
-          <div className="col-12 col-md-12 bg-danger main">
+          <div className="col-12 col-md-12 main">
             <Describe />
             <ButtonSection />
           </div>
