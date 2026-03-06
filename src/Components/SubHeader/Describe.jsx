ca579bc9d5dfe751e9ea8cf4053cbd335cfa6c94
Kiumars609 <qumarsgamer@gmail.com>
2026-03-06 23:03:53 +0400

    Edit Style Describe for SubHeader

--- src/Components/SubHeader/Describe.jsx
+++ src/Components/SubHeader/Describe.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function Describe() {
   return (
     <>
-      <h1 className="sub-header title">
-        Discover The Best Scape Rooms Near You
-      </h1>
+      <div className="sub-header">
+        <h1 className="title">Discover The Best Scape Rooms Near You</h1>
+      </div>
     </>
   );
 }
