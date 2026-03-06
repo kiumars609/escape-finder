69915746eba40f9ed57287245180d4bdb014e688
Kiumars609 <qumarsgamer@gmail.com>
2026-03-06 23:22:12 +0400

    Edit Button Section for SubHeader

--- src/Components/SubHeader/ButtonSection.jsx
+++ src/Components/SubHeader/ButtonSection.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function ButtonSection() {
   return (
-    <div className="col-12 col-md-6 bg-warning button-section mt-5">
-      <button className="btn btn-primary text-white col-12 col-md-4 px-5 rounded-3">
+    <div className="col-12 col-md-6 button-section mt-5">
+      <button className="btn btn-primary text-white col-12 col-md-4 px-5 rounded-3 py-3 fs-5">
         Explore Now
       </button>
 
-      <button className="btn btn-primary text-white col-12 col-md-4 px-5">
+      <button className="btn border-secondary glass-btn col-12 col-md-4 px-5 rounded-3 py-3 fs-5 btn-outline-secondary">
         View Favorites
       </button>
     </div>
