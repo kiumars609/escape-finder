a353618aa00b2f6fcb4b2430e3062b7f7a028fcb
Kiumars609 <qumarsgamer@gmail.com>
2026-03-06 23:07:31 +0400

    Edit Describe for SubHeader

--- src/Components/SubHeader/Describe.jsx
+++ src/Components/SubHeader/Describe.jsx
@@ -4,7 +4,8 @@ export default function Describe() {
   return (
     <>
       <div className="sub-header">
-        <h1 className="title">Discover The Best Scape Rooms Near You</h1>
+        <h1 className="title">Discover The Best Escape Rooms Near You</h1>
+        <h4>Find, compare and book top-rated escape experiences worldwide.</h4>
       </div>
     </>
   );
