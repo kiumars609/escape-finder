45d777775e172750b868107bf9d7fb7e8929b72e
Kiumars609 <qumarsgamer@gmail.com>
2026-03-06 22:52:34 +0400

    Add SubHeader

---
+++ src/Components/SubHeader/SubHeader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+export default function SubHeader() {
+  return (
+    <>
+      <div className="container-fluid sub-header">
+        <div className="row">
+          <div className="col-12 col-md-12 bg-danger main">a</div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
+import SubHeader from "../../Components/SubHeader/SubHeader";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <SubHeader />
     </>
   );
 }
