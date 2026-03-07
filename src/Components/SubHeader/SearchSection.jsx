8a5906910815ddd286569fbed72f2dce5d89b7c9
Kiumars609 <qumarsgamer@gmail.com>
2026-03-07 11:38:54 +0400

    Edit Search Section for SubHeader

--- src/Components/SubHeader/SearchSection.jsx
+++ src/Components/SubHeader/SearchSection.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function SearchSection() {
   return (
-    <div>SearchSection</div>
+    <>
+    <div className="col-12 col-md-10 mt-5 search-section rounded-2">
+        test
+    </div>
+    </>
   )
 }
