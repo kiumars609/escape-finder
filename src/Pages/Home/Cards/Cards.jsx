63d8fe0c6c56bc37aac7da192b635df66b854cee
Kiumars609 <qumarsgamer@gmail.com>
2026-03-07 15:14:20 +0400

    Add Cards

---
+++ src/Pages/Home/Cards/Cards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.css";
+
+export default function Cards() {
+  return (
+    <>
+    <div className="container-fluid show-cards">
+        <div className="row">a</div>
+    </div>
+    </>
+  )
+}
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
-import SubHeader from "../../Components/SubHeader/SubHeader";
+import SubHeader from "./SubHeader/SubHeader";
+import Cards from "./Cards/Cards";
 
 export default function Home() {
   return (
     <>
       <Header />
       <SubHeader />
+      <Cards />
     </>
   );
 }
