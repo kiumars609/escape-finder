182dadc4b6e768e32fac0bcea3c18d9ef4125ddd
Kiumars609 <qumarsgamer@gmail.com>
2026-03-08 13:44:03 +0400

    Edit Card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -4,7 +4,12 @@ import "./style.css";
 export default function Card() {
   return (
     <>
-      <div className="my-card rounded-3 card">test</div>
+      <div className="my-card rounded-3 card d-flex">
+        <div className="col-12 image bg-warning">a</div>
+        <div className="col-12 title bg-success">a</div>
+        <div className="col-12 rate bg-primary">a</div>
+        <div className="col-12 booking bg-white">a</div>
+      </div>
     </>
   );
 }
