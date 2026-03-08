61d55d5cc09e4a56ffb2af8af31fe3fac32ce048
Kiumars609 <qumarsgamer@gmail.com>
2026-03-08 14:26:54 +0400

    Add EscapeRooms Data

---
+++ src/Data/RoomsData.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function RoomsData() {
+  const data = [
+    {
+      id: 1,
+      title: "seven",
+      main_image: "../assets/images/escaperooms/seven/main.png",
+    },
+  ];
+  return data;
+}
