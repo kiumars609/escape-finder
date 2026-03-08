f69e0e1560d9328287e14bcb4c2d84cdbdf2ba07
Kiumars609 <qumarsgamer@gmail.com>
2026-03-08 13:37:19 +0400

    Edit Card

--- src/Pages/Home/Cards/Cards.jsx
+++ src/Pages/Home/Cards/Cards.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
 import "./style.css";
-import Card from '../../../Components/Card/Card';
+import Card from "../../../Components/Card/Card";
 
 export default function Cards() {
   return (
     <>
-    <div className="row col-12 col-md-10 mx-auto show-cards bg-danger">
-        {/* <div className="row bg-danger d-flex"> */}
-            
-        {/* </div> */}
-        <div className="col-12 col-md-4"><Card /></div>
-        <div className="col-12 col-md-4"><Card /></div>
-        <div className="col-12 col-md-4"><Card /></div>
-    </div>
+      <div className="row col-12 col-md-10 mx-auto show-cards bg-danger g-3">
+        <div className="col-12 col-md-4">
+          <Card />
+        </div>
+        <div className="col-12 col-md-4">
+          <Card />
+        </div>
+        <div className="col-12 col-md-4">
+          <Card />
+        </div>
+      </div>
     </>
-  )
+  );
 }
