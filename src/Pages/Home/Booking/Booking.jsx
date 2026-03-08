d13e6137f75b386ad3811acf30d6e20fe76cdac6
Kiumars609 <qumarsgamer@gmail.com>
2026-03-08 17:31:44 +0400

    Edit Booking Section in Home

--- src/Pages/Home/Booking/Booking.jsx
+++ src/Pages/Home/Booking/Booking.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
 import "./style.css";
 
 export default function Booking() {
   return (
     <>
-    <div className="col-12 col-md-12 booking-section">
-        test
-    </div>
+      <div className="col-12 col-md-12 booking-section">
+        <div className="box">
+          <h2>Ready To Escape?</h2>
+          <p>Start Your Adventure Today.</p>
+          <button>Book Now</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
