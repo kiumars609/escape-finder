1f18fc8a9113844b72624048bfc9814f28aeab9a
Kiumars609 <qumarsgamer@gmail.com>
2026-03-09 22:18:59 +0400

    Add Footer

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import SubHeader from "./SubHeader/SubHeader";
 import Cards from "./Cards/Cards";
 import roomsData from "../../Data/RoomsData";
 import Booking from "./Booking/Booking";
+import Footer from "../../Components/Footer/Footer";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <SubHeader />
       <Cards roomsData={roomsData} />
       <Booking />
+      <Footer />
     </>
   );
 }
