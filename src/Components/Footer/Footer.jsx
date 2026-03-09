604713226f15ceface584774343c3da63500016f
Kiumars609 <qumarsgamer@gmail.com>
2026-03-09 22:57:27 +0400

    Edit Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,12 +5,12 @@ export default function Footer() {
   return (
     <>
     <div className="container-fluid">
-        <div className="row bg-danger footer">
-            <div className="col-12 bg-warning">
+        <div className="row footer">
+            <div className="col-12 h-75 bg-warning">
                 test
             </div>
-            <div className="col-12 bg-success">
-                test
+            <div className="col-12 h-25 d-flex align-items-center justify-content-center">
+                <h5>© 2026 EscapeFinder. Built with Me.</h5>
             </div>
         </div>
     </div>
