48ab662d06e398468bd2982948f09c2e9bcb9329
Kiumars609 <qumarsgamer@gmail.com>
2026-03-11 23:34:59 +0400

    Edit Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,88 +7,91 @@ export default function Footer() {
     <>
       <div className="container-fluid footer">
       <div className="container">
-        <div className="row gy-4 footer-top">
+
+        <div className="row gy-4 gx-lg-5 footer-top">
+
+          {/* Logo Section */}
           <div className="col-12 col-md-4">
             <img src={logo} alt="logo" className="logo mb-3" />
+
             <h5 className="footer-text">
-              Find the best escape room <br /> experiences near you.
+              Find the best escape room <br />
+              experiences near you.
             </h5>
           </div>
 
-          <div className="col-12 col-md-3 d-flex flex-column">
-            <h5 className="text-white mb-3">Quick Links</h5>
-            <ul className="list-unstyled m-0">
-              <li className="mb-2">
-                <a href="#" className="text-decoration-none text-light">
-                  About Us
-                </a>
-              </li>
-              <li className="mb-2">
-                <a href="#" className="text-decoration-none text-light">
-                  How It Works
-                </a>
-              </li>
-              <li className="mb-2">
-                <a href="#" className="text-decoration-none text-light">
-                  Contact
-                </a>
-              </li>
-              <li className="mb-2">
-                <a href="#" className="text-decoration-none text-light">
-                  FAQs
-                </a>
-              </li>
-            </ul>
-          </div>
-
+          {/* Quick Links */}
           <div className="col-12 col-md-2">
-            <h5 className="text-white mb-3">Connect With Us</h5>
+            <h5 className="footer-title">Quick Links</h5>
+
             <ul className="list-unstyled m-0">
               <li className="mb-2">
-                <a href="#" className="text-light text-decoration-none">
-                  Email
-                </a>
+                <a href="#" className="footer-link">About Us</a>
               </li>
+
               <li className="mb-2">
-                <a href="#" className="text-light text-decoration-none">
-                  Support
-                </a>
+                <a href="#" className="footer-link">How It Works</a>
               </li>
+
               <li className="mb-2">
-                <a href="#" className="text-light text-decoration-none">
-                  Partnership
-                </a>
+                <a href="#" className="footer-link">Contact</a>
+              </li>
+
+              <li className="mb-2">
+                <a href="#" className="footer-link">FAQs</a>
               </li>
             </ul>
           </div>
 
+          {/* Connect With Us */}
           <div className="col-12 col-md-3">
-            <h5 className="text-white mb-3">Follow Us</h5>
+            <h5 className="footer-title">Connect With Us</h5>
+
             <ul className="list-unstyled m-0">
               <li className="mb-2">
-                <a href="#" className="text-light text-decoration-none">
-                  Instagram
-                </a>
+                <a href="#" className="footer-link">Email</a>
               </li>
+
               <li className="mb-2">
-                <a href="#" className="text-light text-decoration-none">
-                  Twitter
-                </a>
+                <a href="#" className="footer-link">Support</a>
               </li>
+
               <li className="mb-2">
-                <a href="#" className="text-light text-decoration-none">
-                  Facebook
-                </a>
+                <a href="#" className="footer-link">Partnership</a>
               </li>
             </ul>
           </div>
+
+          {/* Follow Us */}
+          <div className="col-12 col-md-3">
+            <h5 className="footer-title">Follow Us</h5>
+
+            <ul className="list-unstyled m-0">
+              <li className="mb-2">
+                <a href="#" className="footer-link">Instagram</a>
+              </li>
+
+              <li className="mb-2">
+                <a href="#" className="footer-link">Twitter</a>
+              </li>
+
+              <li className="mb-2">
+                <a href="#" className="footer-link">Facebook</a>
+              </li>
+            </ul>
+          </div>
+
         </div>
 
+        {/* Bottom Copyright */}
         <div className="row footer-bottom">
-          <div className="col-12 d-flex align-items-center justify-content-center">
-            <h5 className="m-0">© 2026 EscapeFinder. Built with Me.</h5>
+          <div className="col-12 d-flex justify-content-center align-items-center">
+            <p className="footer-copy m-0">
+              © 2026 EscapeFinder. Built with Me.
+            </p>
           </div>
         </div>
+
       </div>
     </div>
     </>
