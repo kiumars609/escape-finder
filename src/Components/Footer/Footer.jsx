6a86336abeaaae92fc776b4eb10c5dd882ea7897
Kiumars609 <qumarsgamer@gmail.com>
2026-03-11 23:24:34 +0400

    Edit Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -43,8 +43,52 @@ export default function Footer() {
                   </li>
                 </ul>
               </div>
-              <div className="col-12 col-md-2 bg-danger">b</div>
-              <div className="col-12 col-md-2 bg-danger">c</div>
+              <div className="col-12 col-md-2 bg-danger">
+                <h5 className="text-white mb-3">Connect With Us</h5>
+
+                <ul className="list-unstyled">
+                  <li className="mb-2">
+                    <a href="#" className="text-light text-decoration-none">
+                      Email
+                    </a>
+                  </li>
+
+                  <li className="mb-2">
+                    <a href="#" className="text-light text-decoration-none">
+                      Support
+                    </a>
+                  </li>
+
+                  <li className="mb-2">
+                    <a href="#" className="text-light text-decoration-none">
+                      Partnership
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-12 col-md-2 bg-danger">
+                <h5 className="text-white mb-3">Follow Us</h5>
+
+                <ul className="list-unstyled">
+                  <li className="mb-2">
+                    <a href="#" className="text-light text-decoration-none">
+                      Instagram
+                    </a>
+                  </li>
+
+                  <li className="mb-2">
+                    <a href="#" className="text-light text-decoration-none">
+                      Twitter
+                    </a>
+                  </li>
+
+                  <li className="mb-2">
+                    <a href="#" className="text-light text-decoration-none">
+                      Facebook
+                    </a>
+                  </li>
+                </ul>
+              </div>
               <div className="col-12 col-md-2 bg-danger"></div>
             </div>
           </div>
