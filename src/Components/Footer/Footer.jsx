5a80d7826aec177842c63eefb1919713e7bb8641
Kiumars609 <qumarsgamer@gmail.com>
2026-03-11 23:41:00 +0400

    Edit Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,12 +6,12 @@ export default function Footer() {
   return (
     <>
       <footer className="footer">
-      <div className="container">
+      <div className="container footer-container">
         <div className="row footer-row">
-          <div className="col-12 col-lg-4">
+          <div className="col-12 col-lg-5">
             <div className="footer-brand">
               <img src={logo} alt="EscapeFinder logo" className="footer-logo" />
-              <p className="footer-desc">
+              <p className="footer-desc mb-0">
                 Find the best escape room
                 <br />
                 experiences near you.
@@ -19,9 +19,9 @@ export default function Footer() {
             </div>
           </div>
 
-          <div className="col-6 col-md-4 col-lg-3">
+          <div className="col-6 col-md-4 col-lg-2">
             <h5 className="footer-title">Quick Links</h5>
-            <ul className="footer-list list-unstyled">
+            <ul className="footer-list list-unstyled mb-0">
               <li><a href="#">About Us</a></li>
               <li><a href="#">How It Works</a></li>
               <li><a href="#">Contact</a></li>
@@ -31,7 +31,7 @@ export default function Footer() {
 
           <div className="col-6 col-md-4 col-lg-2">
             <h5 className="footer-title">Connect With Us</h5>
-            <ul className="footer-list list-unstyled">
+            <ul className="footer-list list-unstyled mb-0">
               <li><a href="#">Email</a></li>
               <li><a href="#">Support</a></li>
               <li><a href="#">Partnership</a></li>
@@ -40,7 +40,7 @@ export default function Footer() {
 
           <div className="col-6 col-md-4 col-lg-3">
             <h5 className="footer-title">Follow Us</h5>
-            <ul className="footer-list list-unstyled">
+            <ul className="footer-list list-unstyled mb-0">
               <li><a href="#">Instagram</a></li>
               <li><a href="#">Twitter</a></li>
               <li><a href="#">Facebook</a></li>
@@ -49,7 +49,7 @@ export default function Footer() {
         </div>
 
         <div className="footer-bottom">
-          <p className="footer-copy">© 2026 EscapeFinder. Built with Me.</p>
+          <p className="footer-copy mb-0">© 2026 EscapeFinder. Built with Me.</p>
         </div>
       </div>
     </footer>
