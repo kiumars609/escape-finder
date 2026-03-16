95a8fc712ebb7ff520c3972dfe906b018c25bf69
Kiumars609 <qumarsgamer@gmail.com>
2026-03-17 00:10:05 +0400

    Fix filter rooms

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,15 +6,12 @@ import Booking from "./Booking/Booking";
 import Layout from "../../Components/Layout/Layout";
 
 export default function Home() {
-  // Get City
   const [city, setCity] = useState(false);
 
-  // City Filter
-  const handleCityFilter = (item) => {
-    setCity(item);
-  };
+  const handleCityFilter = (item) =>{
+    setCity(item)
+  }
 
-  // Filter Action
   const cityFilter = city;
   const resultFiltered = cityFilter
     ? roomsData && roomsData.filter((room) => room.city === cityFilter)
