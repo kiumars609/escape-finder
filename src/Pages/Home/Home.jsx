a942d811e5a1098524362384e1e60ea695953ed8
Kiumars609 <qumarsgamer@gmail.com>
2026-03-18 19:04:30 +0400

    Fix filter rooms

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,30 +7,32 @@ import Layout from "../../Components/Layout/Layout";
 
 export default function Home() {
   // const [city, setCity] = useState(false);
-  const [filtered, setFiltered] = useState({
+
+  const [filters, setFilters] = useState({
     city: "",
     players: "",
   });
 
-  const handleFilteredChange = (key, value) => {
-    setFiltered((prev) => ({
+  const handleFilterRooms = (key, value) => {
+    setFilters((prev) => ({
       ...prev,
       [key]: value,
     }));
   };
 
-  const filteredRooms = roomsData.filter((room) => {
-    return (
-      (!filtered.city || room.city === filtered.city) &&
-      (!filtered.players || room.players === filtered.players)
-    );
-  });
+  const filterRooms = roomsData.filter((room) => {
+    return(
+      (!filters.city || room.city === filters.city) &&
+      (!filters.players || room.players === filters.players)
+    )
+  })
+
 
   return (
     <>
       <Layout>
-        <SubHeader handleFilteredChange={handleFilteredChange} />
-        <Cards roomsData={filteredRooms} />
+        <SubHeader handleFilteredChange={handleFilterRooms} />
+        <Cards roomsData={filterRooms} />
         <Booking />
       </Layout>
     </>
