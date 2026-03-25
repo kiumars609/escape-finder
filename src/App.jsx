08e675fd2539275c2e5ee7a9d622c18e7cfdb9f6
Kiumars609 <qumarsgamer@gmail.com>
2026-03-25 12:16:48 +0400

    Add Room Detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // import './App.css'
 
-import Home from "./Pages/Home/Home"
+import Home from "./Pages/Home"
+import Room from "./Pages/Room"
 
 function App() {
 
   return (
     <>
       <Home />
+      <Room />
     </>
   )
 }
---
+++ src/Pages/Room/Room.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../Components/Layout/Layout'
+
+export default function Room() {
+  return (
+    <>
+    <Layout>
+        test
+    </Layout>
+    </>
+  )
+}
