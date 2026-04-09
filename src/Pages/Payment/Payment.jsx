cb2c6323ffea0cc4a53d97523404d266458a50bc
Kiumars609 <qumarsgamer@gmail.com>
2026-04-09 12:57:19 +0400

    HandleSubmit

--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Payment() {
-  return <div>Payment</div>;
+  const show = useLocation();
+
+  const { date, time } = show.state || {};
+  return <div>Payment {date + " " + time}</div>;
 }
