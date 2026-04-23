3e753064e07021e5ffb447310fc099e74f2e1a2d
Kiumars609 <qumarsgamer@gmail.com>
2026-04-24 00:02:34 +0400

    Fix Payment Component

--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -9,31 +9,6 @@ export default function Payment() {
   return (
     <>
       <Layout>
-        <section className="col-md-6 my-5 col-11 mx-auto">
-          <h2>Booked Date: {date + " - " + time}</h2>
-          <h3>Price: ${price}</h3>
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">Phone Number</th>
-                <th scope="col">Email</th>
-                <th scope="col">Team Name</th>
-              </tr>
-            </thead>
-            <tbody className="table-group-divider">
-              <tr>
-                <th scope="row">1</th>
-                <td>{name}</td>
-                <td>{phone}</td>
-                <td>{email}</td>
-                <td>{teamName}</td>
-              </tr>
-            </tbody>
-          </table>
-        </section>
-
         <section className="container payment-page">
           <div className="row g-4">
             {/* Left Side */}
@@ -178,6 +153,16 @@ export default function Payment() {
                   <p>Time: {time}</p>
 
                   <p>Players: 4</p>
+                </div>
+
+                <div className="user-info">
+                  <h5>User Information</h5>
+
+                  <p>Name: {name}</p>
+
+                  <p>Phone Number: {phone}</p>
+
+                  <p>Email: {email}</p>
 
                   <p>Team Name: {teamName}</p>
                 </div>
