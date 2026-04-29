9fab5ec997ce8ff2416f324ade0dbc25773bd67c
Kiumars609 <qumarsgamer@gmail.com>
2026-04-29 11:44:15 +0400

    Add Theme Context

--- src/assets/Context/Theme/ThemeContext.jsx
+++ src/assets/Context/Theme/ThemeContext.jsx
@@ -1,3 +1,13 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const WhiteThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState("lite");
+
+  return (
+    <WhiteThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </WhiteThemeContext.Provider>
+  );
+}
